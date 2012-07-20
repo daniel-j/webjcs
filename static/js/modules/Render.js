88684d4944ea50a0dbfbcc35da60040997c4e80c
daniel-j <daniel.hede@hotmail.com>
2012-07-20 23:23:39 +0200

    removed webgl-2d

--- static/js/modules/Render.js
+++ static/js/modules/Render.js
@@ -33,7 +33,6 @@ modules.Render = (function (global) {
 		this.canvas.width = window.innerWidth;
 		this.canvas.height = window.innerHeight;
 
-		WebGL2D.enable(this.canvas);
 		this.ctx = this.canvas.getContext('2d');
 
 		
